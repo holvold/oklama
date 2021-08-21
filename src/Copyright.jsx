b6ae11764d681a4822b537e80b8d322b62801478
Emil Hölvold <emilholvold@Emils-Air.lan>
2021-08-21 18:49:33 +0200

    first commit

---
+++ src/Copyright.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Copyright.css'
+
+function Copyright() {
+    return (
+      <div>
+          <p className="Name-tag" >
+           © a react project by Emil Hölvold
+          </p>
+      </div>
+    );
+  }
+  
+  export default Copyright
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Copyright from './Copyright'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div className="Start-page">
+      <div className="App-component" >
+        <App />
+      </div>
+      <div className="Copyright-component">
+        <Copyright />
+      </div>
+    </div>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
